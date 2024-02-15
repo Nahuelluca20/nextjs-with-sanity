3f247d21b9e59e65b09654fdb14459ae212bfe09
Nahuel Luca <nahuelluca20@gmail.com>
2024-02-15 18:24:45 -0300

    Add blockquote component and update layout in post.tsx

--- components/post.tsx
+++ components/post.tsx
@@ -5,6 +5,7 @@ import { SanityDocument } from "next-sanity";
 
 import { dataset, projectId } from "@/sanity/env";
 import { SanityImageAssetDocument } from "@sanity/client/stega";
+import { ReactChildren, ReactNode } from "react";
 
 const builder = imageUrlBuilder({ projectId, dataset });
 
@@ -24,28 +25,38 @@ export default function Post({ post }: { post: SanityDocument }) {
   const { title, mainImage, body } = post;
 
   const components = {
+    block: {
+      blockquote: ({ children }: { children: any }) => (
+        <blockquote className="border-l-purple-500 w-full bg-purple-50 py-2">
+          {children}
+        </blockquote>
+      ),
+    },
     types: {
       image: ImageComponent,
     },
   };
 
   return (
-    <main className="container mx-auto prose prose-lg p-4">
-      {title ? <h1>{title}</h1> : null}
-      {mainImage ? (
-        <Image
-          className="float-left m-0 w-1/3 mr-4 rounded-lg"
-          src={builder
-            .image(mainImage)
-            .width(300)
-            .height(300)
-            .quality(80)
-            .url()}
-          width={300}
-          height={300}
-          alt={mainImage.alt || ""}
-        />
-      ) : null}
+    <main className="container mx-auto prose prose-lg px-4 py-20">
+      <div className="flex flex-row-reverse mb-20">
+        {title ? <h1>{title}</h1> : null}
+        {mainImage ? (
+          <Image
+            className="m-0 w-1/3 mr-4 rounded-lg"
+            src={builder
+              .image(mainImage)
+              .width(300)
+              .height(300)
+              .quality(80)
+              .url()}
+            width={300}
+            height={300}
+            alt={mainImage.alt || ""}
+          />
+        ) : null}
+      </div>
+
       {body ? <PortableText value={body} components={components} /> : null}
     </main>
   );
