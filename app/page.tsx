10ff65529478233a67371052d98cd095988a54c8
Nahuel Luca <nahuelluca20@gmail.com>
2024-02-18 18:35:40 -0300

    Update links in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,20 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center mt-20 min-h-screen">
       <h1 className="text-2xl">Proof of Concept for Next.js with Sanity CMS</h1>
-      <Link
-        href={"/posts"}
-        className="mt-10 bg-indigo-500 text-white p-2 rounded-md"
-      >
-        View posts
-      </Link>
+      <div className="mt-10 space-x-2">
+        <Link
+          href={"/posts"}
+          className="bg-indigo-500 text-white p-2 rounded-md"
+        >
+          View posts
+        </Link>
+        <Link
+          href={"/studio"}
+          className="bg-indigo-900 text-white p-2 rounded-md"
+        >
+          Go to CMS
+        </Link>
+      </div>
     </main>
   );
 }
